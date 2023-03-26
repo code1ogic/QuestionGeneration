9ca42fd32d2d0640e1bb5295cb3541e5264cc4f4
code1ogic <96641204+code1ogic@users.noreply.github.com>
2023-03-26 13:30:16 +0530

    Update questions.component.ts

--- frontend/src/app/component/questions/questions.component.ts
+++ frontend/src/app/component/questions/questions.component.ts
@@ -13,6 +13,7 @@ export class QuestionsComponent implements OnInit {
   keywords : string[] = [];
   summrized_text : string = '';
   questions : any;
+  word_count : number = 0;
   constructor(private dataService : DataService, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
@@ -29,8 +30,9 @@ export class QuestionsComponent implements OnInit {
     let object = {text : this.text, wordlength : 100}
     this.dataService.getQustions(object).subscribe(res => {
       console.log(res);
+      this.word_count = this.text.split(' ').length;
       this.keywords = res.keywords;
-      // this.summrized_text = res.summarized_text;
+      this.summrized_text = res.summarized_text;
       // this.questions = res.questions;
       this.spinner.hide();
     }, err => {

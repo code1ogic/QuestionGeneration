5c9696eb23ef916fbd13ee2cd2ecea83546de0d6
Dev Bots <techtalkwithprajyot@gmail.com>
2023-04-15 12:24:38 +0530

    Added code to support file upload and generate summary

--- frontend/src/app/component/questions/questions.component.ts
+++ frontend/src/app/component/questions/questions.component.ts
@@ -9,19 +9,31 @@ import { DataService } from 'src/app/shared/data.service';
 })
 export class QuestionsComponent implements OnInit {
 
-  text : string = '';
+  text : any = '';
   keywords : string[] = [];
   summrized_text : string = '';
-  questions : any;
+  questions : any[] = [];
   word_count : number = 0;
   constructor(private dataService : DataService, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.keywords = [];
+    this.summrized_text = '';
+    this.questions = [];
+    this.word_count = 0;
   }
   
   file:any;
-  fileChanged(e) {
+  fileChanged(e : any) {
       this.file = e.target.files[0];
+      if(this.file !== undefined ) {
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => {
+          this.ngOnInit();
+          this.text = fileReader.result;
+        }
+        fileReader.readAsText(this.file);
+      }
   }
 
   generateQuestions() {
@@ -30,27 +42,44 @@ export class QuestionsComponent implements OnInit {
       alert('Please enter valid text.')
       return;
     }
-    if(this.file !== undefined ) {
-      let fileReader = new FileReader();
-      fileReader.onload = (e) => {
-        this.text = fileReader.result;
-      }
-      fileReader.readAsText(this.file);
-    }
     
     this.spinner.show();
     let object = {text : this.text, wordlength : 100}
     this.dataService.getQustions(object).subscribe(res => {
-      console.log(res);
       this.word_count = this.text.split(' ').length;
-      this.keywords = res.keywords;
+      this.formatResult(res);
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  generateSummary() {
+    if(this.text == '' && this.file === undefined) {
+      alert('Please enter valid text.')
+      return;
+    }
+    
+    this.spinner.show();
+    let object = {text : this.text, wordlength : 100}
+    this.dataService.getSummary(object).subscribe(res => {
+      this.word_count = this.text.split(' ').length;
       this.summrized_text = res.summarized_text;
-      this.summrized_text.replace(/\[.*?\]/g,"");
-      this.questions = res.questions;
+      this.summrized_text = this.summrized_text.replace(/\[.*?\]/g,"");
       this.spinner.hide();
     }, err => {
       console.log(err);
     })
+  }
+
+  formatResult(res: any) {
+    this.keywords = res.keywords;
+    this.summrized_text = res.summarized_text;
+    this.summrized_text = this.summrized_text.replace(/\[.*?\]/g,"");
+    this.questions = res.questions;
+    this.questions.forEach(q => {
+      q = q.toString().replace(/\[.*?\]/g,"");
+    })
+    this.spinner.hide();
   }
 
 }
